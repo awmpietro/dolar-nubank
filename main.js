d7029383c3b856b76a377f5f62ba2ece8a444699
Arthur Mastropietro <awmpietro@gmail.com>
2019-03-14 12:40:37 -0300

    Aparentemente o banco central só libera a cotação do dia anterior

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function($) {
     $("#calculate-btn").click(function(){
         $.LoadingOverlay("show");
-        var today = moment().format('MM-DD-YYYY');
+        var today = moment().subtract('1', 'days').format('MM-DD-YYYY');
         var url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+today+"'&$top=100&$format=json&$select=cotacaoVenda";
         $.getJSON( url, function( data ) {
             $.LoadingOverlay("hide");
@@ -12,7 +12,8 @@
                 var sum3 = sum1 + sum2;
                 var sum4 = (sum3/100) * 6.38;
                 var sum5 = sum4 + sum3;
-                $('#total').val(sum5.toFixed(4));
+                var total = sum5.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'})
+                $('#total').val(total);
             } else {
                 bootbox.alert("Cotação do dólar comercial não disponibilizada pelo BCB para o dia de hoje.");
             }

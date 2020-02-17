421c6fdbc592429be624224909225033c41a9f44
Arthur Mastropietro <awmpietro@gmail.com>
2020-02-17 13:41:13 -0300

    Suporte para cotação do dólar retroativa

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 (function($) {
+    $('#dolarInfo').css("display", "none");
     $("#calculate-btn").click(function(){
-        $.LoadingOverlay("show");
-        //let today = moment().subtract('3', 'days').format('MM-DD-YYYY');
-        let today = moment().format('MM-DD-YYYY');
-        let url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+today+"'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao";
-        $.getJSON( url, function( data ) {
-            $.LoadingOverlay("hide");
-            if(data.value.length > 0) {
-                let dolar = $('#dolar').val();
-                let sum1 = dolar * data.value[0].cotacaoVenda;
-                let sum2 = (sum1/100) * 4;
-                let sum3 = sum1 + sum2;
-                let sum4 = (sum3/100) * 6.38;
-                let sum5 = sum4 + sum3;
-                let total = sum5.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'})
-                $('#total').val(total);
-                $('#data').html(moment(data.value[0].dataHoraCotacao).format("DD/MM/YYYY HH:mm:ss"));
-                $('#compra').html(data.value[0].cotacaoCompra.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'}))
-                $('#venda').html(data.value[0].cotacaoVenda.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'}))
-            } else {
-                bootbox.alert("Cotação do dólar comercial não disponibilizada pelo BCB para o dia de hoje.");
-            }
-        });
+        getData();
     });
 })(jQuery);
+
+const getData = async () => {
+    $.LoadingOverlay("show");        
+        let res = []
+        let day = 0;
+        while (res.length === 0) {
+            res = await fetchData(day);
+            day++
+        }
+        $.LoadingOverlay("hide");
+        $('#dolarInfo').css("display", "block");
+        let dolar = $('#dolar').val();
+        let sum1 = dolar * res[0].cotacaoVenda;
+        let sum2 = (sum1/100) * 4;
+        let sum3 = sum1 + sum2;
+        let sum4 = (sum3/100) * 6.38;
+        let sum5 = sum4 + sum3;
+        let total = sum5.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'})
+        $('#total').val(total);
+        $('#data').html(moment(res[0].dataHoraCotacao).format("DD/MM/YYYY HH:mm:ss"));
+        $('#compra').html(res[0].cotacaoCompra.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'}));
+        $('#venda').html(res[0].cotacaoVenda.toLocaleString('pt-BR', { style: 'currency', currency: 'REA'}));       
+}
+
+const fetchData = async (day) => {
+    let today = moment().subtract(day, 'days').format('MM-DD-YYYY');
+    let url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+today+"'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao";
+    const data = await $.getJSON( url );
+    return data.value;
+}
